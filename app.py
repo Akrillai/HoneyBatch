c98b88d50585cb864c9aa20c3d27c4594722546e
Mark-Lenovo <markfencingman@mail.ru>
2023-04-07 09:47:52 +0100

    bash.exe + Working Setup

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def execute_workflow():
 
     command = workflow[command_index]
     try:
-        result = subprocess.run(['wsl.exe ' + command], check=True, stdout=subprocess.PIPE, text=True, shell=True)
+        result = subprocess.run(['bash.exe', '-c', command], check=True, stdout=subprocess.PIPE, text=True)
         return jsonify({"status": "success", "output": result.stdout.strip()})
     except subprocess.CalledProcessError as e:
         return jsonify({"status": "error", "output": e.stdout.strip() if e.stdout else 'Error occurred during execution'})
